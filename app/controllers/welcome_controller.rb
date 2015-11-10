8a57d383d5b3a6916b1a00102e237ae82e66b247
Michael Cheng <mcheng.work@gmail.com>
2015-11-11 00:28:47 +0800

    Add a custom OG header for conferences section.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  layout 'conferences', only: [:conferences]
+
   def index
     @recent_episodes = Episode.limit(8).order('published_at DESC')
   end
