199c54ce573d8a1444831176e04e75b3863e15cc
Casie Lane <casielane@gmail.com>
2016-03-05 15:47:09 +0800

    Added controller for subscriber to Mailchimp

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,8 +1,17 @@
 class SubscribersController < ApplicationController
 
   def create
-    puts params
-    render plain:"yeah #{params}"
+  	email = params[:email]
+  	first_name = params[:first_name]
+
+    result = MailchimpService.new.subscribe(email, first_name)
+	if result
+		# flash message! You are in!
+		redirect_to root_path, notice: "Welcome to the mailing list!"
+	else
+		# oh no!
+		redirect_to root_path, error: "Sorry, something went wrong."
+	end
   end
 
 
