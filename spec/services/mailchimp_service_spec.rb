23eca8b3b729cf30f8605686edaf01ebd1626574
Michael Cheng <mcheng.work@gmail.com>
2016-03-05 11:25:01 +0800

    Added test for MailchimpService class.

---
+++ spec/services/mailchimp_service_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe MailchimpService do
+  let(:email) { 'me@example.com' }
+  let(:first_name) { 'Steve' }
+  let(:last_name) { 'Jobs' }
+
+  subject {
+    MailchimpService.new
+  }
+
+  xdescribe 'subscribe' do
+    it 'returns true' do
+      expect(subject.subscribe(email, first_name, last_name)).to be
+    end
+
+    xdescribe 'validation' do
+      describe 'bad email' do
+        let(:bad_email) { 'bad&&&&@example' }
+
+        it 'returns false' do
+          expect(subject.subscribe(bad_email, first_name, last_name)).not_to be
+        end
+      end
+      describe 'no email' do
+        let(:no_email) { '' }
+
+        it 'returns false' do
+          expect(subject.subscribe(no_email, first_name, last_name)).not_to be
+        end
+      end
+      describe 'no first name' do
+        let(:no_first_name) { '' }
+
+        it 'returns false' do
+          expect(subject.subscribe(email, no_first_name, last_name)).not_to be
+        end
+      end
+    end
+  end
+end
+
