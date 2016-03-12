a2d8eeb26c4795f1d788085c7a53aab14aa9fb5c
Michael Cheng <mcheng.work@gmail.com>
2016-03-12 15:01:00 +0800

    Fix typo in variable used.

--- app/services/mailchimp_service.rb
+++ app/services/mailchimp_service.rb
@@ -4,7 +4,7 @@ class MailchimpService
     gibbon = Gibbon::Request.new(api_key: ENV["MAILCHIMP_API_KEY"])
     gibbon.lists(ENV["MAILCHIMP_LIST_ID"]).members
       .create(body: {
-        email_address: email_address,
+        email_address: email,
         status: "subscribed",
         merge_fields: { FNAME: first_name }
       })
