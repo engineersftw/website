d29a1a3f30334f53d2e8055d48aa40a9bc30f284
Michael Cheng <mcheng.work@gmail.com>
2016-03-12 14:47:52 +0800

    Fix syntax error.

--- app/services/mailchimp_service.rb
+++ app/services/mailchimp_service.rb
@@ -2,7 +2,12 @@ class MailchimpService
 
   def subscribe(email, first_name)
     gibbon = Gibbon::Request.new(api_key: ENV["MAILCHIMP_API_KEY"])
-    gibbon.lists(ENV["MAILCHIMP_LIST_ID"]).members.create(body: {email_address:"#{:email_address}", first_name:"#{:first_name}, status: "subscribed")
+    gibbon.lists(ENV["MAILCHIMP_LIST_ID"]).members
+      .create(body: {
+        email_address: email_address,
+        status: "subscribed",
+        merge_fields: { FNAME: first_name }
+      })
   end
 
 end
