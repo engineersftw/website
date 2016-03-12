d70e6c0ab3cac59814a5c50e4972aaab35f45146
Michael Cheng <mcheng.work@gmail.com>
2016-03-12 15:42:19 +0800

    Made the MailchimpService class more readable.

--- app/services/mailchimp_service.rb
+++ app/services/mailchimp_service.rb
@@ -2,7 +2,9 @@ class MailchimpService
 
   def subscribe(email, first_name)
     gibbon = Gibbon::Request.new(api_key: ENV["MAILCHIMP_API_KEY"])
-    gibbon.lists(ENV["MAILCHIMP_LIST_ID"]).members
+    gibbon
+      .lists(ENV["MAILCHIMP_LIST_ID"])
+      .members
       .create(body: {
         email_address: email,
         status: "subscribed",
