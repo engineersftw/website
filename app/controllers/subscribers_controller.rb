9f12bc1fbf7b803528b3103d1239ba640a2d2b11
Michael Cheng <mcheng.work@gmail.com>
2016-03-12 15:42:43 +0800

    Show actual Mailchimp error detail.

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,18 +1,15 @@
 class SubscribersController < ApplicationController
 
   def create
-  	email = params[:email]
-  	first_name = params[:first_name]
+    email = params[:email]
+    first_name = params[:first_name]
 
-    result = MailchimpService.new.subscribe(email, first_name)
-	if result
-		# flash message! You are in!
-		redirect_to root_path, notice: "Welcome to the mailing list!"
-	else
-		# oh no!
-		redirect_to root_path, alert: "Sorry, something went wrong."
-	end
+    begin
+      MailchimpService.new.subscribe(email, first_name)
+      redirect_to root_path, notice: "Welcome to the mailing list!"
+    rescue Gibbon::MailChimpError => e
+      redirect_to root_path, alert: e.detail
+    end
   end
-
 
 end
