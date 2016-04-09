4743bac7b0ee0f5f9e49628a364a3403a1eb8bfa
Michael Cheng mcheng.work@gmail.com <pair+mc@techladies.co>
2016-04-09 09:21:25 +0800

    Make Playlists editable.

--- app/admin/playlist.rb
+++ app/admin/playlist.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Playlist do
-  permit_params :id, :playlist_id, :name, :description, :publish_date, :active
+  permit_params :id, :playlist_category_id, :playlist_id, :name, :description, :website, :hashtag, :image, :publish_date, :active
 
   config.sort_order = 'publish_date_desc'
 
@@ -10,8 +10,8 @@ ActiveAdmin.register Playlist do
     id_column
     column :publish_date
     column :playlist_category
-    column 'Thumbnail' do |org|
-      image_tag org.image, width: 150
+    column 'Thumbnail' do |pl|
+      image_tag pl.image, width: 150
     end
     column :name
     column :active
