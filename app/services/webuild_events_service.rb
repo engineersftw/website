4e02c40b67d225ec0e0b6421c8df367b31e134bf
Michael Cheng mcheng.work@gmail.com <pair+mc@techladies.co>
2016-04-09 14:35:45 +0800

    Use Rails cache. Made changes to copy.

--- app/services/webuild_events_service.rb
+++ app/services/webuild_events_service.rb
@@ -1,9 +1,11 @@
 class WebuildEventsService
   def get_events(event_url='https://webuild.sg/api/v1/events')
     begin
-      response = RestClient.get event_url
-      response_json = JSON.parse(response.body)
-      response_json["events"]
+      Rails.cache.fetch('all_events') do
+        response = RestClient.get event_url
+        response_json = JSON.parse(response.body)
+        response_json["events"]
+      end
     rescue
       []
     end
