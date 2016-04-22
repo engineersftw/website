6d8ef335a6dedaa66755bd9c5c14b1adccb9468a
Michael Cheng mcheng.work@gmail.com <pair+mc@techladies.co>
2016-04-23 00:00:48 +0800

    Add helper to display Twitter message
    - To append presenter's Twitter handle if it exists.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def twitter_share_text(episode)
+    string = episode.title
+    if episode.presenters.count > 0
+      episode.presenters.each do |presenter|
+        string += " @#{presenter.twitter}" if presenter.twitter.present?
+      end
+    end
+
+    string
+  end
 end
